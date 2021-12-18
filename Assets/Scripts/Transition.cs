0668b518d1ff744c4e2b90180d15b9127eaca73d
Andrew Bliss <andrewgbliss@gmail.com>
2021-12-18 17:25:20 +0900

    updates

--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Experimental.Rendering.Universal;
 
+
 public enum TweenLoopType { Clamp, Once, PingPong };
 
 [AddComponentMenu("RFG/Transitions/Transition")]
